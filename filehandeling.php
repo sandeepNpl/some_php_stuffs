a77590c48d94b1c0c5a1e0494b84cee10a580218
Sandip <sandeep47nepali@gmail.com>
2022-08-14 21:12:22 +0545

    added filehandeling

--- filehandeling.php
+++ filehandeling.php
@@ -1,7 +1,15 @@
 <?php
 //read file()
+// // echo readfile("data.txt")
+// ;
 
+// fopen
 
+$mydata = fopen("data.txt","r");
+// echo fread($mydata,filesize("data.txt"));
+// fclose($mydata);
 
-
+//fgets 
+echo fgets($mydata);
+fclose($mydata);
 ?>
